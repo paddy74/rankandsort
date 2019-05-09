986286ba7aebf8391f8df7f8a3f3f40d1f912871
Patrick Cox <coxpatrick74@gmail.com>
2019-05-09 15:16:47 -0400

    Update testData

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -6,27 +6,29 @@
 int main()
 {
     auto data = getTestData();
+    auto const & queryString = data.first;
+    auto & docVect = data.second;
 
     std::cout << "Unsorted:" << std::endl;
-    for (auto const & doc : data.second)
-        {
-            if (doc.count("id") == 0)
-                std::cout << "noid" << ' ';
-            else
-                std::cout << doc.at("id") << ':' << doc.at("rscore") << ' ';
-        }
+    for (auto const & doc : docVect)
+    {
+        if (doc.count("id") == 0)
+            std::cout << "noid" << ' ';
+        else
+            std::cout << doc.at("id") << ':' << doc.at("rscore") << ' ';
+    }
     std::cout << std::endl;
 
-    rankandsort::rankandsort(data.first, data.second);
+    rankandsort::rankandsort(queryString, docVect);
 
     std::cout << "\nSorted:" << std::endl;
-    for (auto const & doc : data.second)
-        {
-            if (doc.count("id") == 0)
-                std::cout << "noid" << ' ';
-            else
-                std::cout << doc.at("id") << ':' << doc.at("rscore") << ' ';
-        }
+    for (auto const & doc : docVect)
+    {
+        if (doc.count("id") == 0)
+            std::cout << "noid" << ' ';
+        else
+            std::cout << doc.at("id") << ':' << doc.at("rscore") << ' ';
+    }
     std::cout << std::endl;
 
     return 0;
