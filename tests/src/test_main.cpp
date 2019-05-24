66e384f1652ff04393f7cf810552f0c8f65edff3
Patrick Cox <coxpatrick74@gmail.com>
2019-05-24 17:20:06 -0400

    Rename test_main->example

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <rankandsort/rankandsort.hpp>
 
 #include "testData.hpp"
+
 
 int main()
 {
@@ -9,27 +9,7 @@ int main()
     auto const & queryString = data.first;
     auto & docVect = data.second;
 
-    std::cout << "Unsorted:" << std::endl;
-    for (auto const & doc : docVect)
-    {
-        if (doc.count("id") == 0)
-            std::cout << "noid" << ' ';
-        else
-            std::cout << doc.at("id") << ':' << doc.at("rscore") << ' ';
-    }
-    std::cout << std::endl;
-
     rankandsort::rankandsort(queryString, docVect);
-
-    std::cout << "\nSorted:" << std::endl;
-    for (auto const & doc : docVect)
-    {
-        if (doc.count("id") == 0)
-            std::cout << "noid" << ' ';
-        else
-            std::cout << doc.at("id") << ':' << doc.at("rscore") << ' ';
-    }
-    std::cout << std::endl;
 
     return 0;
 }
